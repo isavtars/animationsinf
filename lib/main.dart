b8aac44bf515d682b7ec0da938df1e733805b588
isavtars <113200845+isavtars@users.noreply.github.com>
2023-04-25 14:44:22 +0545

    Tweenanimation

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,61 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  // late Animation<Color> animation;
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 3500));
+    // animation = Tween(begin: 0.0, end: 1.0).animate(animationController);
+    animation =
+        CurvedAnimation(parent: animationController, curve: Curves.bounceInOut);
+
+    animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        animationController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        animationController.forward();
+      }
+    });
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: const Placeholder());
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: AnimationLogo(
+            animation: animation,
+          ),
+        ));
+  }
+}
+
+class AnimationLogo extends AnimatedWidget {
+  final Tween<double> _sizeAnim = Tween<double>(begin: 30.0, end: 100.0);
+  AnimationLogo({Key? key, required Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable as Animation<double>;
+    return Transform.scale(
+        scale: animation.value * 50,
+        child: const FlutterLogo(
+          size: 50,
+        ));
   }
 }
